9df9b425b5f3f8d463b4b69e0053d138e423c399
Louis_twt <louistse5283@gmail.com>
2025-03-26 21:46:58 +0000

    setup dark mode

--- src/components/home/auth-screen/AuthScreen.tsx
+++ src/components/home/auth-screen/AuthScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import HeroSection from './HeroSection';
+import { ModeToggle } from '@/components/ModeToggle';
 
 const AuthScreen = () => {
   return (
     <div className="flex flex-col">
+      <ModeToggle />
       <HeroSection />
     </div>
   )
