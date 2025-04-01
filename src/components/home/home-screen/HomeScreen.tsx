f34e0625b0de368b3c4998fb477124d5fe86b7e9
Louis_twt <louistse5283@gmail.com>
2025-04-01 22:04:22 +0100

    completed Sidebar, began on home-screen and related components

--- src/components/home/home-screen/HomeScreen.tsx
+++ src/components/home/home-screen/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import BaseLayout from "@/components/BaseLayout";
+import CoverImage from "./CoverImage";
 
 const HomeScreen = () => {
   return (
-    <BaseLayout>
-      Home Screen cotents
-    </BaseLayout>
+    <div className="flex flex-col">
+      <CoverImage />
+
+    </div>
   )
 }
 
